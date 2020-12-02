ccb8038c21a89327cf39aaae4dddaeab1e0db32a
Ron B <me@ronthecookie.me>
2020-12-02 13:45:50 +0200

    AoC 2020 d2p2

--- aoc-2020/src/day2.rs
+++ aoc-2020/src/day2.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
 
 const RAW_INPUT: &str = include_str!("../inputs/day2");
+const REGEX: &str = r"(?m)^(\d+)-(\d+) ([[:alpha:]]): ([[:alpha:]]+)";
 
 pub fn run() {
     part_1();
@@ -9,7 +10,7 @@ pub fn run() {
 
 fn part_1() {
     let lines = RAW_INPUT.split('\n');
-    let re = Regex::new(r"(?m)^(\d+)-(\d+) ([[:alpha:]]): ([[:alpha:]]+)").unwrap();
+    let re = Regex::new(REGEX).unwrap();
     let mut match_count = 0;
     for line in lines {
         let cap = re.captures(line).unwrap();
@@ -30,4 +31,26 @@ fn part_1() {
     println!("Part 1: {}", match_count)
 }
 
-fn part_2() {}
+fn part_2() {
+    let lines = RAW_INPUT.split('\n');
+    let re = Regex::new(REGEX).unwrap();
+    let mut match_count = 0;
+
+    for line in lines {
+        let cap = re.captures(line).unwrap();
+        let expected_pos = cap.get(1).unwrap().as_str().parse::<usize>().unwrap() - 1;
+        let unexpected_pos = cap.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1;
+        let letter = cap.get(3).unwrap().as_str().chars().nth(0).unwrap();
+        let pw: Vec<char> = cap.get(4).unwrap().as_str().chars().collect();
+        let mut letter_count = 0;
+        for x in &[unexpected_pos, expected_pos] {
+            if pw.get(*x).unwrap() == &letter {
+                letter_count += 1;
+            }
+        }
+        if letter_count == 1 {
+            match_count += 1;
+        }
+    }
+    println!("Part 2: {}", match_count)
+}
