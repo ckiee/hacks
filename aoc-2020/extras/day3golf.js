c7c53f06f83a699f0e55011f081768262cfdfd6d
Ron B <me@ronthecookie.me>
2020-12-03 23:05:23 +0200

    save 5 more bytes

--- aoc-2020/extras/day3golf.js
+++ aoc-2020/extras/day3golf.js
@@ -321,7 +321,7 @@ const t = `.#.......#...........#.........
 ......###....##............#..#
 .........#....#......#.........
 ..........#...............#..#.`;
-// this is 203 bytes:
+// this is 198 bytes:
 d=t.split`\n`.map(x=>[...x].map(y=>y=="#"))
 a=[[1,1],[1,3],[1,5],[1,7],[2,1]].map(([h,j])=>{x=y=c=0;while(x<d.length-1)x+=h,y+=j,d[x][y%d[x].length]&&c++;return c})
-console.log(a[1],a.reduce((a,b)=>a*b))
+console.log(a[1],eval(a.join`*`))
