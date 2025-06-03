2fa2976e90060318f1c8af6c16394d10d719c0fd
ckie <git-525ff67@ckie.dev>
2025-06-03 09:50:59 +0300

    minifluxkl: *; reformat, refine page previewer

--- minifluxkl/index.js
+++ minifluxkl/index.js
@@ -4,7 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const $ = document.querySelector.bind(document);
     const $$ = (...r) => [...document.querySelectorAll(r)];
 
-    if (location.pathname.match(/^\/(history|unread|feed\/\d+\/entries(?:\/\w+)?)$/)) {
+    if (
+        location.pathname.match(
+            /^\/(history|unread|feed\/\d+\/entries(?:\/\w+)?)$/
+        )
+    ) {
         const ZOOM = 1.25;
         const USE_EXT = true;
 
@@ -70,68 +74,90 @@ body {
         viewer.classList.add("viewer");
 
         const preview = document.createElement("div");
-        preview.innerHTML = `<div class="spinner">🐈</div>`
+        preview.innerHTML = `<div class="spinner">🐈</div>`;
 
         let iframe = document.createElement("iframe");
         let bgIframe = document.createElement("iframe");
         bgIframe.classList.add("xkl-bg");
-        [iframe, bgIframe].map(fr => fr.addEventListener("load", () => fr.setAttribute("data-loaded", true)))
+        [iframe, bgIframe].map((fr) =>
+            fr.addEventListener("load", () =>
+                fr.setAttribute("data-loaded", true)
+            )
+        );
 
         preview.appendChild(iframe);
         preview.appendChild(bgIframe);
         preview.classList.add("preview");
 
         // Reparent .items to be under new .viewer under main
-        main.replaceChild(viewer, items)
+        main.replaceChild(viewer, items);
         viewer.appendChild(items);
         viewer.appendChild(preview);
 
         const articles = $$(".items > article");
 
         let focused;
-        const viewUrlOf = a => {
-            const ext = a.querySelector(".item-meta-icons-external-url a").getAttribute("href");
+        const viewUrlOf = (a) => {
+            const ext = a
+                .querySelector(".item-meta-icons-external-url a")
+                .getAttribute("href");
             const int = a.querySelector(".item-title a").getAttribute("href");
             let useExt = USE_EXT;
-            if (new URL(ext).hostname == "cloudwithlightning.net") useExt = false;
+            if (new URL(ext).hostname == "cloudwithlightning.net")
+                useExt = false;
+            if (new URL(ext).hostname == "www.tumblr.com") useExt = false;
+            if (
+                ["mei.puppycat.house", "pupcat-dev.tailnet.ckie.dev"].includes(
+                    new URL(ext).hostname
+                ) &&
+                new URL(ext).pathname == "/visual"
+            )
+                useExt = false;
+            if (new URL(ext).hostname.match(/(\.?|^)substack\.com$/))
+                useExt = false;
             // if (new URL(ext).hostname.match(/(?:^|\.)dreamwidth\.org$/)) useExt = false;
 
             return useExt ? ext : int;
         };
 
-        articles.map(article =>
-            article.onfocus = e => {
-                if (focused !== article) {
-                    articles.map(e => e.classList.remove("current-item"));
-                    article.classList.add("current-item");
-                    const idx = articles.indexOf(article);
+        articles.map(
+            (article) =>
+                (article.onfocus = (e) => {
+                    if (focused !== article) {
+                        articles.map((e) => e.classList.remove("current-item"));
+                        article.classList.add("current-item");
+                        const idx = articles.indexOf(article);
 
-                    let ofs = idx == 0 ? 0 : -articles[idx - 1].offsetHeight;
-                    if (article !== articles[0]) {
-                        article.scrollIntoView(true)
-                        window.scrollBy({ top: ofs });
+                        let ofs =
+                            idx == 0 ? 0 : -articles[idx - 1].offsetHeight;
+                        if (article !== articles[0]) {
+                            article.scrollIntoView(true);
+                            window.scrollBy({ top: ofs });
+                        }
+                        preview.style.transform = `translateY(${
+                            article.offsetTop - preview.offsetTop + ofs
+                        }px)`;
+                        if (viewUrlOf(article) == bgIframe.src) {
+                            const previouslyPrimary = iframe;
+                            iframe = bgIframe;
+                            bgIframe = previouslyPrimary;
+
+                            previouslyPrimary.classList.add("xkl-bg");
+                            iframe.classList.remove("xkl-bg");
+                            console.log("SWAP!");
+                        } else {
+                            iframe.src = "about:blank";
+                            iframe.removeAttribute("data-loaded");
+                        }
+                        iframe.src = viewUrlOf(article);
+                        bgIframe.removeAttribute("data-loaded");
+                        bgIframe.src = viewUrlOf(
+                            articles[articles.indexOf(article) + 1]
+                        );
                     }
-                    preview.style.transform = `translateY(${article.offsetTop - preview.offsetTop + ofs}px)`;
-                    if (viewUrlOf(article) == bgIframe.src) {
-                        const previouslyPrimary = iframe;
-                        iframe = bgIframe;
-                        bgIframe = previouslyPrimary;
-
-                        previouslyPrimary.classList.add("xkl-bg");
-                        iframe.classList.remove("xkl-bg");
-                        console.log("SWAP!")
-                    } else {
-                        iframe.src = "about:blank";
-                        iframe.removeAttribute("data-loaded");
-                    }
-                    iframe.src = viewUrlOf(article)
-                    bgIframe.removeAttribute("data-loaded");
-                    bgIframe.src = viewUrlOf(articles[articles.indexOf(article) + 1]);
-                }
-                focused = article;
-            }
-        )
-
+                    focused = article;
+                })
+        );
 
         articles[0].focus(); // only works if the _page_ is focused (FF on X11)
         articles[0].onfocus();
